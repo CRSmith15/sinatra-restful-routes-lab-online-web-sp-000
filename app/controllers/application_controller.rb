598f96fcf4e0ca6ce90b367f8ea08c5bd2df0b31
Chance Smith <chancesmith97@hotmail.com>
2020-03-16 00:08:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,7 +52,8 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/recipes/:id/delete' do
-    Recipe.find(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
     redirect "/recipes/"
   end
 
